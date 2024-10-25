2892a705ea00ff7936f7f48fd6ab24a5c44e1228
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-25 08:58:22 +0600

    Linked List

--- Remove Duplicates from Sorted List II.py
+++ Remove Duplicates from Sorted List II.py
@@ -5,3 +5,30 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        numbers_range = [0] * 201
+
+        temp = head
+
+        while temp:
+            num = temp.val
+            offset_val = num + 100
+            numbers_range[offset_val] += 1
+            temp = temp.next
+        
+        new_head = None
+        temp = None
+
+        for i in range(len(numbers_range)):
+            freq = numbers_range[i]
+            if freq > 1:
+                continue
+            elif freq == 1:
+                if temp:
+                    tail_node = ListNode(i-100)
+                    temp.next = tail_node
+                    temp = temp.next
+                else:
+                    new_head = ListNode(i-100)
+                    temp = new_head
+        
+        return new_head
