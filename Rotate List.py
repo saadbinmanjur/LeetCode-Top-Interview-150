78de1c2066a99c8ee95d0446bda0af174b0b4b6e
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-26 06:10:40 +0600

    Linked List

--- Rotate List.py
+++ Rotate List.py
@@ -5,4 +5,32 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        
+        if not head:
+            return head
+
+        n = self.length(head)
+        if k % n == 0:
+            return head
+
+        k = k % n
+
+        node = head
+        for _ in range(n - k - 1):
+            node = node.next
+
+        res = node.next
+        node.next = None
+
+        tail = res
+        while tail.next:
+            tail = tail.next
+
+        tail.next = head
+        return res
+
+    def length(self, node: Optional[ListNode]) -> int:
+        n = 0
+        while node:
+            node = node.next
+            n += 1
+        return n
