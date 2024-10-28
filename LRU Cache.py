d02c5c20dc2278a98b6846fbb09571a0da66bff5
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-28 20:36:56 +0600

    Linked List

--- LRU Cache.py
+++ LRU Cache.py
@@ -1,13 +1,20 @@
+from collections import OrderedDict
+
 class LRUCache:
-
-    def __init__(self, capacity: int):
-        
-
-    def get(self, key: int) -> int:
-        
-
-    def put(self, key: int, value: int) -> None:
-        
+    def __init__(self,capacity):
+        self.cache=OrderedDict()
+        self.capacity=capacity
+    def get(self,key):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        else:
+            return -1
+    def put(self, key,value):
+        self.cache[key]=value
+        self.cache.move_to_end(key)
+        if len(self.cache)>self.capacity:
+            self.cache.popitem(last=False)
 
 
 # Your LRUCache object will be instantiated and called as such:
