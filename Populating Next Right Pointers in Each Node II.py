825558e672944b3215c19ec8e2a6a83f4b5cda05
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-04 06:41:24 +0600

    Binary Tree General

--- Populating Next Right Pointers in Each Node II.py
+++ Populating Next Right Pointers in Each Node II.py
@@ -10,4 +10,20 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        
+        if root is None:
+            return root
+        queue = deque()
+        queue.append(root)
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                cur = queue.popleft()
+                if i < size-1:
+                    cur.next = queue[0]
+                else:
+                    cur.next = None
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+        return root
