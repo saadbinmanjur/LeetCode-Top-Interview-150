e69a96ff8265c6101d79416f65db2855f9b92b2b
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-05 22:52:21 +0600

    Binary Tree General

--- Flatten Binary Tree to Linked List.py
+++ Flatten Binary Tree to Linked List.py
@@ -1,12 +1,21 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def flatten(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        
+    def linkedlist(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return None
+
+        left = self.linkedlist(root.left)
+        right = self.linkedlist(root.right)
+
+        if left is not None:
+            trav = left
+            while trav.right is not None:
+                trav = trav.right
+            trav.right = right
+
+            root.right = left
+            root.left = None
+
+        return root
+
+    def flatten(self, root: TreeNode) -> None:
+        self.linkedlist(root)
