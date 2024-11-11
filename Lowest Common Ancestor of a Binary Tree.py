c1cce351b747b233d43161076aa491b8fe3c3498
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-11 06:11:54 +0600

    Binary Tree General

--- Lowest Common Ancestor of a Binary Tree.py
+++ Lowest Common Ancestor of a Binary Tree.py
@@ -6,8 +6,8 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or root == p or root == q:
+  def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if not root or root == p or root == q:
       return root
 
     l = self.lowestCommonAncestor(root.left, p, q)
