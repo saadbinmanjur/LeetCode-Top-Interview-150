26d06575414c67d862187273ae82d2a0cedd8eca
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-13 22:46:24 +0600

    Binary Tree BFS

--- Average of Levels in Binary Tree.py
+++ Average of Levels in Binary Tree.py
@@ -6,4 +6,20 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        q = deque()
+        q.append(root)
+
+        res = []
+        while q:
+            num = len(q)
+            avg = []
+            for i in range(len(q)):
+                x = q.popleft()
+                if x:
+                    avg.append(x.val)
+                    q.append(x.left)
+                    q.append(x.right)
+            if avg:
+                res.append(sum(avg)/len(avg))
         
+        return res
