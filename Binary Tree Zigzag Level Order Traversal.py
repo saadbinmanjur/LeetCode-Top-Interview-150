f68c8c2827429cb8e71e3aa0737581bcfa518543
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-15 20:51:58 +0600

    Binary Tree BFS

--- Binary Tree Zigzag Level Order Traversal.py
+++ Binary Tree Zigzag Level Order Traversal.py
@@ -7,5 +7,26 @@
 
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        my_dict = defaultdict(list)
+
+        def dfs(root, height):
+            if not root:
+                return
+            
+            height += 1
+            my_dict[height].append(root.val)
+
+            dfs(root.left, height)
+            dfs(root.right, height)
+
+            return my_dict
         
+        dfs(root, 0)
+        result = []
+        if my_dict:
+            for k, v in sorted(my_dict.items()):
+                if k % 2 == 0:
+                    v.reverse()
+                result.append(v)
+        return result
         
