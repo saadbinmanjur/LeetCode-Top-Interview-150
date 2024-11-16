5884529269e89180fe6e196cc955badd72086cdc
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-16 23:06:26 +0600

    Binary Search Tree

--- Minimum Absolute Difference in BST.py
+++ Minimum Absolute Difference in BST.py
@@ -6,4 +6,27 @@
 #         self.right = right
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return []
+
+        values = []
+        differences = []
+
+        def dfs(node: Optional[TreeNode]):
+            if node:
+                values.append(node.val)
+                dfs(node.left)
+                dfs(node.right)
         
+        dfs(root)
+        values = sorted(values)
+
+        if len(values) <= 1:
+            return []
+            
+        for i in range(1, len(values)):
+            differences.append(
+                values[i] - values[i-1]
+            )
+
+        return min(differences)
