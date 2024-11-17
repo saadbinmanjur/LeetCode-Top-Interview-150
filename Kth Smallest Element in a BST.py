444220fbc2ecd764050b56bb7281523d0325dcbe
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-17 21:10:19 +0600

    Binary Search Tree

--- Kth Smallest Element in a BST.py
+++ Kth Smallest Element in a BST.py
@@ -6,4 +6,13 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
+        if not root:
+            return
+        self.inorder(root.left, ans)
+        ans.append(root.val)
+        self.inorder(root.right, ans)
+
+    def kthSmallest(self, root, k):
+        ans = []
+        self.inorder(root, ans)
+        return ans[k - 1]
