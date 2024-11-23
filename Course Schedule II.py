ff76822957ce3ddaba0adc56a95bf3c408c7e5c4
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-23 11:17:10 +0600

    Graph General

--- Course Schedule II.py
+++ Course Schedule II.py
@@ -1,3 +1,31 @@
+from collections import defaultdict, deque
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+        
+        in_degree = [0] * numCourses
+        for course, prereq in prerequisites:
+            in_degree[course] += 1
+        
+        queue = deque()
+        for i in range(numCourses):
+            if in_degree[i] == 0:
+                queue.append(i)
+        
+        result = []
+        while queue:
+            node = queue.popleft()
+            result.append(node)
+            for neighbor in graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        if len(result) < numCourses:
+            return []
+        
+        return result
