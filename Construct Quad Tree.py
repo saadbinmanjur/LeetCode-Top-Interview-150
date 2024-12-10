dfafeaf01583b2f983b29c1491460e8e8c9ce5ab
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-12-10 21:44:50 +0600

    Divide & Conquer

--- Construct Quad Tree.py
+++ Construct Quad Tree.py
@@ -12,4 +12,12 @@ class Node:
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
+        if all(cell == grid[0][0] for row in grid for cell in row):
+            return Node(bool(grid[0][0]), True)
         
+        # if not, return a node with all smaller grids
+        n = len(grid) // 2
+        return Node(True, False, self.construct([row[:n] for row in grid[:n]]),
+                                 self.construct([row[n:] for row in grid[:n]]),
+                                 self.construct([row[:n] for row in grid[n:]]),
+                                 self.construct([row[n:] for row in grid[n:]]))
