7779caef15878bc77129267e69c077ee144c7223
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-12-20 20:58:35 +0600

    Binary Search

--- Median of Two Sorted Arrays.py
+++ Median of Two Sorted Arrays.py
@@ -1,3 +1,9 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
+        nums1 += nums2
+        nums1.sort()
+        Len = len(nums1)
+        if Len % 2 == 0:
+            return (nums1[int(Len / 2)] + nums1[int((Len / 2) - 1)]) /2
+        else:
+            return nums1[int((Len - 1) / 2)]
