fdad5ce585ce5b9836a81a136939e1587cb77037
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-01-03 16:47:00 +0600

    Math

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -1,3 +1,11 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        
+        if x <= 1:
+            return x
+
+        iter = (x // 2) + 2 
+        for i in range(1,iter):
+            if i**2 == x:
+                return i
+            if i*i > x:
+                return i - 1
