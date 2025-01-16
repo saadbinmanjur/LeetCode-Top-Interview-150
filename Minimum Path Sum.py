26722523fc1288f6d461923e2c28885cad65e2d8
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-01-16 11:35:18 +0600

    Multidimensional DP

--- Minimum Path Sum.py
+++ Minimum Path Sum.py
@@ -1,3 +1,13 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
+        n, m = len(grid), len(grid[0])
+        for i in range(0, n):
+            for x in range(0, m): 
+                if i > 0 and x > 0: 
+                    grid[i][x] += min(grid[i-1][x], grid[i][x-1])
+                elif i > 0:
+                    grid[i][x] += grid[i-1][x]
+                elif x > 0: 
+                    grid[i][x] += grid[i][x-1]
+
+        return grid[n-1][m-1]
